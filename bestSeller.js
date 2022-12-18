aa99e29c05208c3a55b68dc217fd11b6458109a4
shivam5665 <shivamsharmaji04@gmail.com>
2022-12-19 02:36:11 +0530

    goodnight

--- bestSeller.js
+++ bestSeller.js
@@ -1,25 +1,32 @@
 
 let container = document.getElementById("container")
 
+let gif = document.getElementById("load");
 
 
 function appendData(data){
-    
+    container.innerHTML = null;
     data.forEach((ele)=>{
         let card = document.createElement("div");
         card.setAttribute("class","cardDiv");
+        card.style.paddingLeft = "10px"
         let image = document.createElement("img");
         image.src = ele.img_src;
-        image.style.width = "100%"
+        image.style.width = "100%";
+        
         let title = document.createElement("p");
-        title.innerText = ele.items_p;
+        title.innerText = ele.items_p.substr(1,50);
+
         let price = document.createElement("h2");
-        price.innerText = `Є+${ele.items_price}`;
+        price.innerText = `₹${ele.items_price}`;
         let fav = document.createElement('img');
         fav.src = `./images/heart(3).png`;
-        fav.style.width = "7%";
+        fav.style.width = "30%";
+        fav.style.marginTop = "10px"
+        fav.style.height = "25px";
         let fav_count = document.createElement('p');
-        fav_count.innerText = ele.favite_count;
+        fav_count.innerText =  +ele.favite_count;
+        
         let freeship =  document.createElement('p');
         freeship.innerText = ele.free_ship;
         let last = document.createElement("div");
@@ -30,6 +37,7 @@ function appendData(data){
         };
         
         small.append(fav,fav_count);
+        small.style.gap ="5px"
         last.append(small);
         small.setAttribute("id","small");
         last.setAttribute("id","last");
@@ -46,12 +54,10 @@ function appendData(data){
 
 }
 
-
-async function collectcard(){
-
-
+let asc = document.getElementById("lth");
+asc.onclick = async () => {
     try{
-        let response = await fetch("http://localhost:3000/Best_Seller");
+        let response = await fetch(`http://localhost:3000/Best_Seller?_page=1&_limit=15_sort=items_price&_order=desc`);
         let data = await response.json();
         console.log(data)
         appendData(data)
@@ -60,4 +66,22 @@ async function collectcard(){
         console.log(err)
     }
 }
-collectcard()
+let dsc = document.getElementById("htl");
+
+
+async function collectcard(){
+    load.style.display = "none";
+
+    try{
+        let response = await fetch("http://localhost:3000/Best_Seller?_page=1&_limit=15");
+        let data = await response.json();
+        console.log(data)
+        appendData(data)
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+
+setInterval(collectcard, 3000);
