040c84b697810af65109e36ce553e081873df92a
Shahid Afridi <siddiquibabuddin202@gmail.com>
2022-12-18 12:32:58 +0530

    day5 done

---
+++ page8.js
@@ -0,0 +1,79 @@
+let bag=[];
+let cartitem = JSON.parse(localStorage.getItem("cartproduct8"))||[];
+
+let url = "https://636b32aeb10125b78fed84ae.mockapi.io/page-4";
+fetch(url)
+.then((res) =>  res.json())
+.then((data) => {
+    bag = data
+    console.log(data)
+    displayTable(data)
+})
+.catch((err) => alert("something went wrong"))
+
+function displayTable(array){
+    console.log(array)
+    document.querySelector("#container8").innerHTML="";
+    array.forEach((element) => {
+                
+    let div=document.createElement("div")
+
+    let img=document.createElement("img")
+    img.setAttribute("src",element.image)
+
+    // let name=document.createElement("h5")
+    // name.innerText=element.name;
+
+    let detail=document.createElement("p")
+    detail.innerText=element.pro_detail.substring(1,50);
+
+    let id=document.createElement("h4")
+    id.innerText=`id: ${element.id}`;
+    
+    let price=document.createElement("h4")
+    price.innerText=`Price:- ${element.price}`;
+
+    let review=document.createElement("h4")
+    review.innerText=`rating: ${element.review_num}`;
+
+    // let shiping=document.createElement("p")
+    // shiping.innerText=`ship: ${element.free_ship}`;
+
+
+    let button=document.createElement("button")
+    button.innerText="add to cart"
+    button.addEventListener("click",function(){
+        let isPresent = false;
+
+        for (let i = 0; i < cartitem.length; i++ ) {
+                 
+               if (cartitem[i] == element) {
+                  
+                    alert("item is already present in the cart");
+
+                    isPresent = true;
+
+                    break;
+
+               }
+
+        }
+
+        if (!isPresent) {
+
+            alert("Item added to the cart")
+
+            cartitem.push(element)
+            localStorage.setItem("cartproduct8",JSON.stringify(cartitem))
+
+
+        }
+        
+        
+        
+    })
+
+    div.append(img,detail,id,price,review,button)
+    document.querySelector("#container8").append(div)
+    });   
+}
